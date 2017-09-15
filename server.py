14b95ddd6a1329f2a0eb8df92585e4f6b49a7056
宮澤賢生 <yotio@yochio-3.local>
2017-09-15 14:27:45 +0900

    first commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from bottle import route, run, request,response,HTTPResponse
+import json
+
+@route('/')
+def hello():
+    return "<html><body><h1>Hello!!</h1></body></html>"
+
+@route('/huffman', method='POST')
+def huffman():
+    data = json.load(request.body)
+    print(data['data'])
+    body = {"status":"OK", "message":"Received datas!! Thank you!!"}
+    r = HTTPResponse(status=200, body=body)
+    r.set_header("Content-Type","application/json")
+    return r
+
+
+
+run(host='localhost', port=8000, debug=True)
